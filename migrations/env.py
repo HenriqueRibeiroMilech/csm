277d97a317d1bf00b58dda6c27a17ead9f31405e
Henrique Milech <devhenriquemilech@gmail.com>
2025-09-12 18:35:16 -0300

    Rename DATABASE_URL to ASYNC_DATABASE_URL for consistency in async database configuration

--- migrations/env.py
+++ migrations/env.py
@@ -14,7 +14,7 @@ from backend.settings import Settings
 
 
 config = context.config
-config.set_main_option('sqlalchemy.url', Settings().DATABASE_URL)
+config.set_main_option('sqlalchemy.url', Settings().ASYNC_DATABASE_URL)
 
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
